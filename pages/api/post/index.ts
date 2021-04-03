321e2121f86b21f314da3a2d51a5589a9260edf3
strocchia <scotttrocchia@gmail.com>
2021-04-03 11:28:51 -0400

    update prisma schema so post has createdAt and updatedAt timestamps

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -8,12 +8,14 @@ export default async function handle(req, res) {
   const { title, content } = req.body;
 
   const session = await getSession({ req });
+
   const result = await prisma.post.create({
     data: {
       title: title,
       content: content,
-      author: { connect: { email: session?.user?.email } },
+      author: { connect: { email: session.user.email } },
     },
   });
+
   res.json(result);
 }
